c825d0a646edc5d67829760e41fe3e36d2146b77
eh-th <ehret@ikmail.com>
2022-06-07 07:41:02 +0200

    Create basic math functions and an operate function

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(a,b) {
+    return a+b
+}
+
+function substract(a,b) {
+    return a-b
+}
+
+function multiply(a,b) {
+    return a*b
+}
+
+function divide(a,b) {
+    return a/b
+}
+
+function operate(operator, a, b) {
+    if (operator = "+") {
+        add(a,b)
+    } else if (operator = "-") {
+        substract(a,b)
+    } else if (operator = "*") {
+        multiply(a,b)
+    } else if (operator = "/") {
+        divide(a,b)
+    }
+}
